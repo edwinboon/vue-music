5be802f5ac921cb922f6fd3fdb6e23f3069ce8fd
Edwin Boon <edwin.boon@eo.nl>
2022-01-26 13:22:50 +0100

    Add boilerplate code

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/tailwind.css'
+import './assets/main.css'
 
-createApp(App).use(store).use(router).mount('#app')
+// initialize instance
+const app = createApp(App)
+
+// register plugins
+app.use(store)
+app.use(router)
+
+// mount app
+app.mount('#app')
