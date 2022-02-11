64e985c7f2d9d01be59100065d8ee4fd78aa0c20
Edwin Boon <edwin.boon@eo.nl>
2022-02-11 15:23:05 +0100

    Rendering songs list with songs from firebase

---
+++ src/types/Song.ts
@@ -0,0 +1,10 @@
+export interface Song {
+  comment_count: number
+  display_name: string
+  genre: string
+  modified_name: string
+  original_name: string
+  uid: string
+  url: string
+  id: string
+}
--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -11,230 +11,11 @@
           <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
             <span class="card-title">My Songs</span>
             <i
-              class="fa fa-compact-disc float-right text-green-400 text-2xl"
+              class="fa fa-compact-disc float-right text-purple-400 text-2xl"
             ></i>
           </div>
           <div class="p-6">
-            <!-- Composition Items -->
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-red-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-times"></i>
-                </button>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-blue-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-              <div>
-                <form>
-                  <div class="mb-3">
-                    <label class="inline-block mb-2">Song Title</label>
-                    <input
-                      type="text"
-                      class="
-                        block
-                        w-full
-                        py-1.5
-                        px-3
-                        text-gray-800
-                        border border-gray-300
-                        transition
-                        duration-500
-                        focus:outline-none focus:border-black
-                        rounded
-                      "
-                      placeholder="Enter Song Title"
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <label class="inline-block mb-2">Genre</label>
-                    <input
-                      type="text"
-                      class="
-                        block
-                        w-full
-                        py-1.5
-                        px-3
-                        text-gray-800
-                        border border-gray-300
-                        transition
-                        duration-500
-                        focus:outline-none focus:border-black
-                        rounded
-                      "
-                      placeholder="Enter Genre"
-                    />
-                  </div>
-                  <button
-                    type="submit"
-                    class="py-1.5 px-3 rounded text-white bg-green-600"
-                  >
-                    Submit
-                  </button>
-                  <button
-                    type="button"
-                    class="py-1.5 px-3 rounded text-white bg-gray-600"
-                  >
-                    Go Back
-                  </button>
-                </form>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-red-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-times"></i>
-                </button>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-blue-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-red-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-times"></i>
-                </button>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-blue-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-red-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-times"></i>
-                </button>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-blue-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-red-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-times"></i>
-                </button>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-blue-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
+            <songs-list v-for="song in songs" :key="song.id" :song="song"/>
           </div>
         </div>
       </div>
@@ -243,15 +24,43 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import Upload from '@/components/Upload.vue'
+import { defineComponent, ref, onMounted } from "vue";
+import { auth, songsCollection } from "@/includes/firebase";
+import { Song } from '@/types/Song'
+import { firebase } from '@/includes/firebase'
+import Upload from "@/components/Upload.vue";
+import SongsList from '@/components/SongsList.vue'
 
 export default defineComponent({
-  name: 'Manage',
+  name: "Manage",
   components: {
-    Upload
-  }
-})
+    Upload,
+    SongsList,
+  },
+  setup() {
+    const songs = ref<Song[]>([]);
 
+    onMounted(async () => {
+      try {
+        const snapshot = await songsCollection
+          .where("uid", "==", auth?.currentUser?.uid)
+          .get();
+
+        snapshot.forEach((document: firebase.firestore.DocumentData) => {
+          const song: Song = {
+            ...document.data(),
+            id: document.id
+
+          };
+
+          songs.value.push(song)
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    return { songs };
+  },
+});
 </script>
-
