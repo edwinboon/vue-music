6b8abcdb3c558e610ab9870be331cb665571f0aa
Edwin Boon <edwin.boon@eo.nl>
2022-02-22 14:10:17 +0100

    Comment section is working now

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -1,61 +1,91 @@
 <template>
-  <!-- Music Header -->
-  <section class="w-full mb-8 py-14 text-center text-white relative">
-    <div
-      class="absolute inset-0 w-full h-full box-border bg-contain music-bg"
-      style="background-image: url(/assets/img/song-header.png)"
-    ></div>
-    <div class="container mx-auto flex items-center">
-      <!-- Play/Pause Button -->
-      <button
-        type="button"
-        class="
-          z-50
-          h-24
-          w-24
-          text-3xl
-          bg-white
-          text-black
-          rounded-full
-          focus:outline-none
-        "
-      >
-        <i class="fas fa-play"></i>
-      </button>
-      <div class="z-50 text-left ml-8">
-        <!-- Song Info -->
-        <div class="text-3xl font-bold">{{ song?.modified_name }}</div>
-        <div>{{ song?.genre }}</div>
-      </div>
-    </div>
-  </section>
-  <!-- Form -->
-  <section class="container mx-auto mt-6">
-    <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
-        <!-- Comment Count -->
-        <span class="card-title">Comments ({{ song?.comment_count }})</span>
-        <i class="fa fa-comments float-right text-purple-400 text-2xl"></i>
-      </div>
-      <div class="p-6">
-        <div
-          class="text-white text-center font-bold p-4 mb-4"
-          :class="commentAlertVariant"
-          v-if="commentShowAlert"
+  <main>
+    <!-- Music Header -->
+    <section class="w-full mb-8 py-14 text-center text-white relative">
+      <div
+        class="absolute inset-0 w-full h-full box-border bg-contain music-bg"
+        style="background-image: url(/assets/img/song-header.png)"
+      ></div>
+      <div class="container mx-auto flex items-center">
+        <!-- Play/Pause Button -->
+        <button
+          type="button"
+          class="
+            z-50
+            h-24
+            w-24
+            text-3xl
+            bg-white
+            text-black
+            rounded-full
+            focus:outline-none
+          "
         >
-          {{ commentAlertMessage }}
+          <i class="fas fa-play"></i>
+        </button>
+        <div class="z-50 text-left ml-8">
+          <!-- Song Info -->
+          <div class="text-3xl font-bold">{{ song?.modified_name }}</div>
+          <div>{{ song?.genre }}</div>
         </div>
-        <vee-form
-          :validation-schema="commentSchema"
-          @submit="addComment"
-          v-if="isUserLoggedIn"
-        >
-          <vee-field
-            name="comment"
-            as="textarea"
+      </div>
+    </section>
+    <!-- Form -->
+    <section class="container mx-auto mt-6" id="comments">
+      <div
+        class="bg-white rounded border border-gray-200 relative flex flex-col"
+      >
+        <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+          <!-- Comment Count -->
+          <span class="card-title">Comments ({{ song?.comment_count }})</span>
+          <i class="fa fa-comments float-right text-purple-400 text-2xl"></i>
+        </div>
+        <div class="p-6">
+          <div
+            class="text-white text-center font-bold p-4 mb-4"
+            v-if="commentShowAlert"
+            :class="commentAlertVariant"
+          >
+            {{ commentAlertMessage }}
+          </div>
+          <vee-form
+            :validation-schema="commentSchema"
+            @submit="addComment"
+            v-if="isUserLoggedIn"
+          >
+            <vee-field
+              as="textarea"
+              name="comment"
+              class="
+                block
+                w-full
+                py-1.5
+                px-3
+                text-gray-800
+                border border-gray-300
+                transition
+                duration-500
+                focus:outline-none focus:border-black
+                rounded
+                mb-4
+              "
+              placeholder="Your comment here..."
+            ></vee-field>
+            <ErrorMessage class="text-red-600" name="comment" />
+            <button
+              type="submit"
+              class="py-1.5 px-3 rounded text-white bg-purple-600 block"
+              :disabled="commentInSubmission"
+            >
+              Submit
+            </button>
+          </vee-form>
+          <!-- Comment sortinging -->
+          <select
+            v-model="sorting"
             class="
               block
-              w-full
+              mt-4
               py-1.5
               px-3
               text-gray-800
@@ -64,66 +94,35 @@
               duration-500
               focus:outline-none focus:border-black
               rounded
-              mb-4
             "
-            placeholder="Your comment here..."
-          ></vee-field>
-          <div>
-            <error-message class="text-red-600" name="comment" />
-          </div>
-          <button
-            type="submit"
-            class="py-1.5 px-3 rounded text-white bg-purple-600 block"
-            :disabled="commentInSubmission"
           >
-            Submit
-          </button>
-        </vee-form>
-        <!-- Sort Comments -->
-        <select
-          v-model="sort"
-          class="
-            block
-            mt-4
-            py-1.5
-            px-3
-            text-gray-800
-            border border-gray-300
-            transition
-            duration-500
-            focus:outline-none focus:border-black
-            rounded
-          "
-        >
-          <option value="1">Latest</option>
-          <option value="2">Oldest</option>
-        </select>
+            <option value="1">Latest</option>
+            <option value="2">Oldest</option>
+          </select>
+        </div>
       </div>
-    </div>
-  </section>
-  <!-- Comments -->
-  <section>
-  <ul class="container mx-auto" style="margin-bottom: 80px">
-    <li
-      class="p-6 bg-white border border-gray-200"
-      v-for="comment in sortedComments"
-      :key="comment.songId"
-    >
-      <!-- Comment Author -->
-      <div class="mb-5">
-        <div class="font-bold">{{ comment.name }}</div>
-        <time>{{ comment.datePosted }}</time>
-      </div>
+    </section>
+    <!-- Comments -->
+    <ul class="container mx-auto" style="margin-bottom: 80px">
+      <li
+        class="p-6 bg-white border border-gray-200"
+        v-for="comment in sortedComments"
+        :key="comment.songId"
+      >
+        <!-- Comment Author -->
+        <div class="mb-5">
+          <div class="font-bold">{{ comment.name }}</div>
+          <time>{{ comment.datePosted }}</time>
+        </div>
 
-      <p>
-        {{ comment.content }}
-      </p>
-    </li>
-  </ul>
-  </section>
+        <p>{{ comment.content }}</p>
+      </li>
+    </ul>
+  </main>
 </template>
+
 <script lang="ts">
-import { computed, defineComponent, ref, onMounted } from 'vue'
+import { computed, defineComponent, ref, onMounted, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { Song } from '@/types/Song'
 import {
@@ -143,7 +142,7 @@ export default defineComponent({
     const router = useRouter()
     const song = ref<Song>()
     const comments = ref<Comment[]>([])
-    const sort = ref<string>('1')
+    const sorting = ref<string>('1')
     const store: Store = useStore()
 
     const commentInSubmission = ref<boolean>(false)
@@ -151,22 +150,31 @@ export default defineComponent({
     const commentAlertVariant = ref<string>('bg-purple-500')
     const commentAlertMessage = ref<string>('Commment is being submitted.')
 
+    // watch
+    watch(sorting, (newVal) => {
+      if (newVal === route.query.sort) {
+        return
+      }
+      router.push({
+        query: {
+          sort: newVal,
+        },
+      })
+    })
+
     const commentSchema: CommentSchema = {
       comment: 'required|min:3',
     }
 
     const isUserLoggedIn = computed(() => store.state.isLoggedIn)
     const sortedComments = computed(() => {
-      console.log(sort.value)
       return comments.value.slice().sort((a, b) => {
-        if (sort.value === '1') {
-          console.log('b: ', new Date(b.datePosted).getTime())
-          console.log('a: ', new Date(a.datePosted).getTime())
-          return Math.abs(
+        if (sorting.value === '1') {
+          return (
             new Date(b.datePosted).getTime() - new Date(a.datePosted).getTime()
           )
         } else {
-          return Math.abs(
+          return (
             new Date(a.datePosted).getTime() - new Date(b.datePosted).getTime()
           )
         }
@@ -201,6 +209,14 @@ export default defineComponent({
         commentAlertMessage.value = String(error)
 
         return
+      }
+
+      // update comment_count (song could be null but should not)
+      if (song.value) {
+        song.value.comment_count += 1
+        await songsCollection.doc(route.params.id as string).update({
+          comment_count: song.value.comment_count
+        })
       }
 
       // get commmenst
@@ -242,6 +258,10 @@ export default defineComponent({
         return router.push({ name: '404' })
       }
 
+      const { sort } = route.query
+
+      sorting.value = sort === '1' || sort === '2' ? sort : '1'
+
       song.value = {
         id: route.params.id as string,
         comment_count: document.comment_count,
@@ -266,7 +286,7 @@ export default defineComponent({
       commentAlertMessage,
       isUserLoggedIn,
       song,
-      sort,
+      sorting,
       sortedComments,
     }
   },
